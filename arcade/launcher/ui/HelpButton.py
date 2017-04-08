aa7e94dd0950e01418800a8a0cfafed54406c510
John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
2017-04-08 11:56:41 +0200

    New upstream version 2.8.3+dfsg

---
+++ arcade/launcher/ui/HelpButton.py
@@ -0,0 +1,11 @@
+from fsbc.desktop import open_url_in_browser
+from .IconButton import IconButton
+
+
+class HelpButton(IconButton):
+    def __init__(self, parent, url):
+        IconButton.__init__(self, parent, "16/information.png")
+        self.url = url
+
+    def on_activate(self):
+        open_url_in_browser(self.url)
